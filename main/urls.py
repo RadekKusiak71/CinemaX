8ffb3819da67b6f4af444e1a244c9a6649f6dfd0
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-06-07 00:50:57 +0200

    register/login/logout/movies_api urls added

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
 from .import views
 
-urlpatterns = []
+urlpatterns = [
+    path('',views.HomePage.as_view(),name='home_page'),
+    path('register/',views.RegisterRequest.as_view(),name='register_page'),
+    path('login/',views.LoginRequest.as_view(),name='login_page'),
+    path('logout/',views.LogoutRequest.as_view(),name='logout_req'),
+    path('movies_api,',views.MovieListAPI.as_view(),name='movies_api_page'),
+]
