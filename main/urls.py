c2d656d7561134fe7b5f0717c2d4daf4a7da204c
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-06-07 13:30:38 +0200

    Added movie creator page url

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/',views.LoginRequest.as_view(),name='login_page'),
     path('logout/',views.LogoutRequest.as_view(),name='logout_req'),
     path('movies_api,',views.MovieListAPI.as_view(),name='movies_api_page'),
+    path('movies_api/creator/<str:pk>',views.MovieCreator.as_view(),name='movie_creator_page')
 ]
