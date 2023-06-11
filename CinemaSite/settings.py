9c8dfb826c2c11bb847c3eb05d4bee176bd90872
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-06-12 01:55:54 +0200

    Configured mail backend

--- CinemaSite/settings.py
+++ CinemaSite/settings.py
@@ -125,3 +125,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#MAIL BACKEND DATA
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'radoslawkusiak12@gmail.com'
+EMAIL_HOST_PASSWORD = 'cxxsdxoxghvjofrr'
+EMAIL_PORT = 587
